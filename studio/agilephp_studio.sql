73b1c7ecffe3a9da98fd7ae37b0cd4f06da48a72
jeremyhahn <jeremyhahn@9bb1911b-6204-4126-9146-82cb5eeae392>
2010-07-19 04:31:32 +0000

    - Bug fix to studio package introduced by refactoring of user.enabled bit to boolean - Updated agilephp_studio.sql
    git-svn-id: https://agilephp.svn.sourceforge.net/svnroot/agilephp/trunk@147 9bb1911b-6204-4126-9146-82cb5eeae392

--- studio/agilephp_studio.sql
+++ studio/agilephp_studio.sql
@@ -111,7 +111,7 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL,
   `last_login` datetime DEFAULT NULL,
   `roleId` varchar(25) DEFAULT NULL,
-  `enabled` bit(1) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`username`),
   KEY `FK_UserRoles` (`roleId`),
   CONSTRAINT `FK_UserRoles` FOREIGN KEY (`roleId`) REFERENCES `roles` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
@@ -120,7 +120,7 @@ CREATE TABLE `users` (
 /*Data for the table `users` */
 
 /* Passwords are set to "test" !!! */
-insert  into `users`(`username`,`password`,`email`,`created`,`last_login`,`roleId`,`enabled`) values ('admin','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','jeremy.hahn@makeabyte.com','2009-09-06 15:27:44','1969-12-31 19:00:00','admin','1'),('test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','test','2010-01-22 19:01:00','1969-12-31 19:00:00','test',NULL);
+insert  into `users`(`username`,`password`,`email`,`created`,`last_login`,`roleId`,`enabled`) values ('admin','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','root@localhost','2009-09-06 15:27:44','1969-12-31 19:00:00','admin','1'),('test','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','test','2010-01-22 19:01:00','1969-12-31 19:00:00','test',NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
